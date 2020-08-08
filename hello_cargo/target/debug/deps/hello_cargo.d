5a025c1c6120599d2cfad3fbf0a9476d0564860f
edward <3zangelks@gmail.com>
2020-08-09 02:39:11 +0800

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/edward/Documents/code/github.com/Angelki/rust-demos/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/edward/Documents/code/github.com/Angelki/rust-demos/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
