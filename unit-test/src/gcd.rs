98e6fcd17dbcb7273abf9279c95e09e2595a8c57
edward <3zangelks@gmail.com>
2020-10-04 11:01:38 +0800

    add test

--- unit-test/src/gcd.rs
+++ unit-test/src/gcd.rs
@@ -17,14 +17,20 @@ pub fn gcd(mut m: u64, mut n: u64) -> u64 {
     n
 }
 
-// 会改变原始值
-pub fn gcd1(m: &mut u64, n: &mut u64) -> u64 {
-    assert!(*n != 0 && *m != 0);
-    while *m != 0 {
-        if *n > *m {
-            swap(m, n);
-        }
-        *m = *m % *n;
-    }
-    *n
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
 }
+
+// // 会改变原始值
+// pub fn gcd1(m: &mut u64, n: &mut u64) -> u64 {
+//     assert!(*n != 0 && *m != 0);
+//     while *m != 0 {
+//         if *n > *m {
+//             swap(m, n);
+//         }
+//         *m = *m % *n;
+//     }
+//     *n
+// }
