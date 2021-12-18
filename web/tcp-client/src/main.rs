559d73e643142c2e9159f8aee06095a47e558d23
Edward <3zangelks@gmail.com>
2021-12-19 01:49:41 +0800

    test

--- web/tcp-client/src/main.rs
+++ web/tcp-client/src/main.rs
@@ -1,5 +1,5 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::io::{Write, Read};
 use std::str;
 
 fn main() {
@@ -7,5 +7,6 @@ fn main() {
     stream.write("Hello".as_bytes()).unwrap();
     let mut buffer = [0; 5];
     stream.read(&mut buffer).unwrap();
+    println!("Response from Server: {}", str::from_utf8(&buffer).unwrap());
     println!("Response from Server: {}", str::from_utf8(&buffer).unwrap());
 }
